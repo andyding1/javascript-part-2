0d6ae7a0e4c60719ca69e0fa118dc9883818da93
andyding <andy.ding89@gmail.com>
2016-01-28 20:59:28 +0000

    completed addressbook.js with Person and Email constructors with an addEmail on Person.prototype

---
+++ addressbook.js
@@ -0,0 +1,28 @@
+function Person(firstName, lastName){
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.emails = [];
+}
+function Email(address, type){
+    this.address= address;
+    switch(type){
+        case 'home':
+            this.type='home';
+            break;
+        case 'work':
+            this.type='work';
+            break;
+        default:
+            this.type='other';
+    }
+}
+Person.prototype = {
+    addEmail: function(address,type){
+        this.emails.push(new Email(address,type));
+    }
+};
+var person1 = new Person('Andy','Ding');
+var person2 = new Person('Bonnie','Ding');
+person1.addEmail('andyding@yahoo.com','home');
+person1.addEmail('ading@eoriginal.com','work');
+console.log(person1);
